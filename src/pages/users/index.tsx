f6a290a08cdba13746b3c9e6b64259df8937d471
Guilherme Lopes <guilhermelopes17@hotmail.com>
2022-07-26 11:04:50 -0300

    criando hook para a listagem de usuarios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,12 +1,11 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
-import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
-import { api } from "../../services/api";
+import { useUsers } from "../../services/hooks/useUsers";
 
 type User = {
   id: string;
@@ -16,26 +15,7 @@ type User = {
 }
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useQuery(["users"], async () => {
-    const { data } = await api.get('users');
-
-    const users = data.users.map((user: User) => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric'
-        })
-      };
-    });
-
-    return users;
-  }, {
-    staleTime: 1000 * 5, // 5 seconds
-  });
+  const { data, isLoading, isFetching, error } = useUsers();
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -96,7 +76,7 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {data.map((user: User) => {
+                  {data?.map(user => {
                     return (
                       <Tr key={user.id}>
                         <Td px={["4", "4", "6"]}>
